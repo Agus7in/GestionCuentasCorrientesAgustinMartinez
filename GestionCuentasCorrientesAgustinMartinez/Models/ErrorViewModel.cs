8f2f0c3a1a408dc68c0ce3bbddab44369efb9b73
Agustin <agustinmartinez729@gmail.com>
2024-02-26 19:27:03 -0300

    Agregar archivos de proyecto.

---
+++ GestionCuentasCorrientesAgustinMartinez/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionCuentasCorrientesAgustinMartinez.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
